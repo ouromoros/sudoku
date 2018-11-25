8038ea21b76c21ff7f14b95e0d5149cdcaeb0182
monser <ouromoros@gmail.com>
2018-11-25 23:23:45 +0800

    初步完成main函数的输入输出

--- SudokuSolver/SudokuSolver.cpp
+++ SudokuSolver/SudokuSolver.cpp
@@ -3,7 +3,88 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <exception>
+#include <vector>
+
+#include "Generator.h"
+#include "Solver.h"
+
+using namespace std;
+
+void PrintUsage();
+void OutputBoard(ofstream &fout, const Board &b);
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World!\n"; 
+	if (argc != 3) {
+		PrintUsage();
+		return 0;
+	}
+	string option(argv[1]);
+	if (option == "-c") {
+		int n;
+		try {
+			n = stoi(argv[2]);
+		}
+		catch (invalid_argument e) {
+			cout << "'" << argv[2] << "' is not a valid number!";
+			return 0;
+		}
+		Generator g(n);
+		vector<Board> boards = g.GetBoards();
+		ofstream fout("sudoku.txt");
+		if (!fout) {
+			cout << "Failed opening file 'sudoku.txt' !" << endl;
+			return 0;
+		}
+		for (int i = 0; i < n; i++) {
+			OutputBoard(fout, boards[i]);
+		}
+	}
+	else if (option == "-s") {
+		ifstream fin(argv[2]);
+		if (!fin) {
+			cout << "Failed opening file '" << argv[2] << "' !" << endl;
+			return 0;
+		}
+		Board b;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				fin >> b[i][j];
+				if (b[i][j] < 0 || b[i][j] > 9 || fin.fail()) {
+					cout << "Invalid Sudoku problem!" << endl;
+					return 0;
+				}
+			}
+		}
+		Solver s(b);
+		if (s.GetSolutions().empty()) {
+			cout << "No solution exists!" << endl;
+			return 0;
+		}
+		Board solution = s.GetSolutions[0];
+		ofstream fout("sudoku.txt");
+		if (!fout) {
+			cout << "Failed opening file 'sudoku.txt' !" << endl;
+			return 0;
+		}
+		OutputBoard(fout, solution);
+	}
+	else {
+		PrintUsage();
+		return 0;
+	}
+
 }
+
+void PrintUsage() {
+	cout << "Usage:" << endl;
+	cout << "  ./sudoku.exe -c <number_of_solutions>" << endl;
+	cout << "  ./sudoku.exe -s <path_of_output_file>" << endl;
+}
+
+void OutputBoard(ofstream &fout, const Board &b) {
+
+}
+
