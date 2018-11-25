913716b598a8147058716f86c962a169b48ffde1
monser <ouromoros@gmail.com>
2018-11-25 23:24:04 +0800

    完成测试文件的框架

--- SudokuSolverTest/test.cpp
+++ SudokuSolverTest/test.cpp
@@ -1,6 +1,33 @@
 #include "pch.h"
+#include "../SudokuSolver/Solver.h"
+#include "../SudokuSolver/State.h"
+#include "../SudokuSolver/Generator.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(StateTest, EmptyState) {
+
+}
+
+TEST(StateTest, InvalidStateBeNULL) {
+
+}
+
+TEST(SolverTest, RightSolution) {
+
+}
+
+TEST(SolverTest, MultipleSolutions) {
+
+}
+
+TEST(GeneratorTest, RightNumberOfBoards) {
+
+}
+
+TEST(GeneratorTest, NonRepeatBoards) {
+
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
